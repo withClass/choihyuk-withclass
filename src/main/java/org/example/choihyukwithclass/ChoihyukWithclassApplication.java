6f9fd000186f67ce7679e0ae2b0f1bc7e8b6883f
wannabeing <wannabeing@naver.com>
2025-05-20 15:28:15 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/org/example/choihyukwithclass/ChoihyukWithclassApplication.java
@@ -0,0 +1,13 @@
+package org.example.choihyukwithclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoihyukWithclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoihyukWithclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/choihyukwithclass/ChoihyukWithclassApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.choihyukwithclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoihyukWithclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
